1bd7cc8042ab8088c6bb9da9a266bcb53001e1d1
Matthew Easley <matteasley8791@gmail.com>
2021-01-20 16:01:02 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,7 +1,9 @@
 def find_even_values(src)
+  new_array = []
   row_index = 0 
   while row_index < src.count do 
     element_index = 0 
+    smallest_number = 100
     while element_index < src[row_index].count do 
       if src[row_index][element_index].even?
         p src[row_index][element_index]
